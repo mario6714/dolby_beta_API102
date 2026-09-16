b921bc91da83a727b5b84a0d8b2d93c2f62fe5dd
Raziel <32179719+mario6714@users.noreply.github.com>
2026-09-16 11:12:05 -0600

    Update compileSdk and targetSdk to version 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ tasks.whenTaskAdded {
 
 android {
     namespace = "com.raincat.dolby_beta"
-    compileSdk = 34
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.raincat.dolby_beta"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 37
         versionCode = 400
         versionName = "4.0.0"
 
